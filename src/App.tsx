89f6dfeecdf9c47f99afd9fc72bccdd7bf6d9c0f
fornecedortextil <leiterepres@hotmail.com>
2026-07-02 15:14:30 +0000

    configurando caminho de deploy

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Portal Têxtil</h1>
+      <p>Aplicação carregada com sucesso!</p>
+    </div>
+  );
+}
